8fb4dedf978356bd790bf35990315e8783125df7
kimtg <kimtg011017@gmail.com>
2026-01-05 23:20:22 +0900

    feat: 패키지 구조 정리

--- app/src/main/java/com/example/areumdap/UI/MainActivity.kt
+++ app/src/main/java/com/example/areumdap/UI/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.areumdap
+package com.example.areumdap.UI
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.areumdap.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
