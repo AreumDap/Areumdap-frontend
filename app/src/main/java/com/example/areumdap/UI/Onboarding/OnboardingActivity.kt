9ddac3e108bc46457696da111d54ec9a4adff0d4
아린 <leesch587@naver.com>
2026-02-01 18:15:33 +0900

    feat: 상단 뒤로가기 버튼 구현 (#94)

--- app/src/main/java/com/example/areumdap/UI/Onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/areumdap/UI/Onboarding/OnboardingActivity.kt
@@ -31,8 +31,6 @@ class OnboardingActivity : AppCompatActivity() {
         setupNextButtonClickListener()
 
         binding.ivBack.setOnClickListener {
-            Log.d("OnboardingActivity", "ivBack clicked")
-
             handleBackPress()
         }
     }
