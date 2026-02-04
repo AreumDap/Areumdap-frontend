f6761aed7a761b2336b317cf56aabb2cb3236cee
cheshire <sodang0645@gmail.com>
2026-02-04 20:48:56 +0900

    bug: RecommendUiState.kt삭제(#101)

--- app/src/main/java/com/example/areumdap/UI/Home/data/RecommendUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.areumdap.UI.Home.data
-
-import com.example.areumdap.domain.model.question.GetChatbotRecommendsResponse
-
-sealed interface RecommendUiState {
-    data object Loading: RecommendUiState
-    data class Success(val items:List<GetChatbotRecommendsResponse>) : RecommendUiState
-    data class Error(val message:String): RecommendUiState
-}
